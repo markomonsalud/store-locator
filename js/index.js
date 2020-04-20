cb85b56de3e2afd6a8c72e339853e475a249b1e0
markomonsalud <mfgmonsalud@gmail.com>
2020-04-20 20:38:03 +0800

    End of Day 2. gitignore added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
 //            var map;
+var marker;
 
 function initMap() {
     var melbourne = {
         lat: -37.840935, 
         lng: 144.946457
     };
+    var store1 = {
+        lat: -37.74964,
+        lng: 144.861553
+    }
+    
     map = new google.maps.Map(document.getElementById('map'), {
         center: melbourne,
-        zoom: 11,
-        mapTypeId: 'roadmap',
+        zoom: 12,
+        mapTypeId: 'roadmap'
     });
+
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+        position: store1
+    });
+    marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
 }

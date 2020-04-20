9f877e9e23b90523029802c6e676dcda0f1e8d85
markomonsalud <mfgmonsalud@gmail.com>
2020-04-21 05:58:31 +0800

    Added markers

--- js/index.js
+++ js/index.js
@@ -1,34 +1,30 @@
-//            var map;
-var marker;
+var map;
+var markers = [];
+var infoWindow;
 
 // same as window.onload = function() {}
 window.onload = () => {
     displayStores();
+
 }
 
 function initMap() {
+    var losAngeles = {
+        lat: 34.063380, 
+        lng: -118.358080
+    };
     var melbourne = {
         lat: -37.840935, 
         lng: 144.946457
     };
-    var store1 = {
-        lat: -37.74964,
-        lng: 144.861553
-    }
     
     map = new google.maps.Map(document.getElementById('map'), {
-        center: melbourne,
-        zoom: 12,
+        center: losAngeles,
+        zoom: 11,
         mapTypeId: 'roadmap'
     });
 
-    marker = new google.maps.Marker({
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP,
-        position: store1
-    });
-    marker.addListener('click', toggleBounce);
+    showStoresMarkers();
 }
 
 function toggleBounce() {
@@ -39,7 +35,7 @@ function toggleBounce() {
     }
 }
 
-function displayStores(){
+function displayStores() {
 
     var storesHtml = '';
     for (var [index, store] of stores.entries()){
@@ -64,4 +60,37 @@ function displayStores(){
         `
         document.querySelector('.stores-list').innerHTML = storesHtml;
     }
+}
+
+function showStoresMarkers() {
+    var bounds = new google.maps.LatLngBounds();
+    for (var [index, store] of stores.entries()){
+
+        var name = store["name"];
+        var address = store["addressLines"][0];
+        var latlng = new google.maps.LatLng(
+             store["coordinates"]["latitude"],
+             store["coordinates"]["longitude"]);
+        bounds.extend(latlng);
+        createMarker(latlng, name, address,++index);
+    }
+    map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, address,index) {
+
+    var html = "<b>" + name + "</b><br/>" + address;
+    var marker = new google.maps.Marker({
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+        position: latlng,
+        label: index.toString()
+    });
+    // marker.addListener('click', toggleBounce);
+    // google.maps.event.addListener(marker, 'click', function() {
+    //     infoWindow.setContent(html);
+    //     infoWindow.open(map, marker);
+    //   });
+      markers.push(marker);
 }
